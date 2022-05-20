7d511dbe07e912a419993756f62d51e93ca21fd5
Stephen Czekalski <stephencze243@gmail.com>
2022-05-20 14:32:48 -0400

    Working on adding image drag and zoom to ImagePreview component.

--- src/components/ImagePreview/ImagePreview.js
+++ src/components/ImagePreview/ImagePreview.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useRef, useEffect } from "react";
 import "./ImagePreview.scss";
 
 /**
@@ -8,10 +9,74 @@ import "./ImagePreview.scss";
  */
 const ImagePreview = (props) => {
 
+  const imagePreviewRef = useRef(null);
+
+  const [previewData, setPreviewData] = useState({
+    drag: {
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0
+    },
+    zoom: 300
+  });
+
+  const isEventInImagePreview = (event) => {
+    const refOffsetLeft = imagePreviewRef.current.offsetLeft;
+    const refOffsetRight = refOffsetLeft + imagePreviewRef.current.offsetWidth;
+    const refOffsetTop = imagePreviewRef.current.offsetTop;
+    const refOffsetBottom = refOffsetTop + imagePreviewRef.current.offsetHeight
+
+    const targetX = event.offsetX;
+    const targetY = event.offsetY;
+
+    if(targetX >= refOffsetLeft && targetX <= refOffsetRight) {
+      if(targetY >= refOffsetTop && targetY <= refOffsetBottom) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  const handleImageZoom = (event) => {
+    if(isEventInImagePreview(event)) {
+
+    }
+  }
+
+  const handleImageDrag = (event) => {
+    if(isEventInImagePreview(event)) {
+      
+      console.log("VALID");
+    }
+  }
+
+  useEffect(() => {
+
+    document.addEventListener("scroll", handleImageZoom);
+    document.addEventListener("click", handleImageDrag);
+
+    return () => {
+      document.removeEventListener("scroll", handleImageZoom);
+      document.removeEventListener("click", handleImageDrag);
+    }
+
+  });
+
   const getImage = () => {
     if(props.appState.originalImage !== null) {
+
+      const styles = {
+        height: previewData.zoom
+      }
+
       return (
-        <img src={props.appState.originalImage.content} />
+        <img 
+          style={ styles } 
+          src={props.appState.originalImage.content}           
+          alt="what you just uploaded."
+        />
       );
 
     } else {
@@ -22,7 +87,7 @@ const ImagePreview = (props) => {
   }
 
   return (
-    <div className="image-preview-wrapper">
+    <div className="image-preview-wrapper" ref={ imagePreviewRef }>
       <div className="image-preview">
         { getImage() }
       </div>
